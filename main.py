2e55dd0d89bccb5451dd1ebcfc877334dda19959
xsephtion <xseph01@gmail.com>
2020-05-05 13:05:44 +0800

    Init

---
+++ main.py
@@ -0,0 +1,74 @@
+types = ["int", "char", "double", "float", "main", "void", "include", "iostream", "string", "using", "namespace", "std",
+         "return","cout"]
+operators = ["=", "+", "-", "/", "*"]
+symbols = ["(", ")", "{", "}", ";", "#", "<", ">"]
+keywords = ["if", "else", "switch"]
+numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "Hello", "World"]
+varin = ["a-zA-z"]
+
+f = open("test.cpp", "r")
+charread = f.read()
+store = []
+chars = []
+sym = []
+numb = []
+temp = []
+variables = []
+
+for char in charread:
+    for ch in char:
+        if ch == ";" or ch == "<" or ch == ">" or ch == "#":
+            chars.append(' ')
+            chars.append(ch)
+            chars.append(' ')
+        else:
+            chars.append(ch)
+
+for i in chars:
+    if i == ' ' or i == '\n':
+        store.append(''.join(temp))
+        store.append(' ')
+        temp.clear()
+    else:
+        temp.append(i)
+
+for q in chars:
+    if q == "(" or q == ")":
+        sym.append(q)
+    if q == "{" or q == "}":
+        sym.append(q)
+    if q == "<" or q == ">":
+        sym.append(q)
+    if q == ";":
+        sym.append(q)
+    if q == "#":
+        sym.append(q)
+
+for num in chars:
+    if 48 <= ord(num) <= 57:
+        numb.append(num)
+
+
+def loop(list, store, text):
+    count = 0
+    for i in range(len(store)):
+        for q in range(len(list)):
+            if str(store[i]) == list[q]:
+                print(text + str(store[i]))
+                count = count + 1
+    return count
+
+
+total = loop(types, store, 'Identifier: ')
+total = total + loop(operators, store, 'Operation: ')
+total = total + loop(alph, store, "String: ")
+total = total + loop(numerals, numb, 'Integers: ')
+total = total + loop(symbols, sym, 'Symbols: ')
+print("Total count of tokens: " + str(total))
+f.close()
+
+m = open("test.i", "w+")
+m.write(str(store))
+m.close()
